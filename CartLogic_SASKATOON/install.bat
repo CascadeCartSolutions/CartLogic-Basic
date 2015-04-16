d4115a1d8b04aa68ec003aeeecbdfd8a1efec4d5
andytus <andrew.roberts@cascadeng.com>
2015-04-16 11:50:01 -0400

    version 2.5
    added sync filter page to data download
    fixed bug related to changing add-even street filtering
    fixed bug related to verifying selected tickets expected RFID
    fixed bug where the ATID shows it's still logged in, but no sites show
    up
    added logging system to get more accurate error reports

---
+++ CartLogic_SASKATOON/install.bat
@@ -0,0 +1,5 @@
+mkdir "New Tickets"
+mkdir "Completed Tickets"
+mkdir "\Completed Tickets\Pictures"
+start "\Windows\wceload.exe" "\Flash Disk\Start\HardReset\CartLogic\Install SQL\sqlce.wce5.armv4i.CAB"
+start "\Windows\wceload.exe" "\Flash Disk\Start\HardReset\CartLogic\Install SQL\sqlce.dev.ENU.wce5.armv4i.CAB"
